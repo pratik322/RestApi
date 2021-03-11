2c2b366efc6c73afa6e908c03f012e0babe84466
Pratik Khandelwal <Pratik.Khandelwal@philips.com>
2021-03-11 20:20:06 +0530

    Adding the RestApi Solution

---
+++ RestAPIProject/RestAPIProject/GetRestData.cs
@@ -0,0 +1,37 @@
+﻿using RestSharp;
+
+namespace RestAPIProject
+{
+    public class GetRestData
+
+    {
+        public void getApiData(string Path, string FileName)
+        {
+            var client = new RestClient("https://eservices.mas.gov.sg/api/action/datastore/search.json?resource_id=7e181136-d81a-48a8-9350-3f09265db3c7&limit=5");
+            var req = new RestRequest();
+            var response = client.Execute(req);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+
+                string rawResponse = response.Content;
+
+                Aspose.Cells.Workbook workbook = new Aspose.Cells.Workbook();
+                Aspose.Cells.Cells cells = workbook.Worksheets[0].Cells;
+
+                Aspose.Cells.Utility.JsonLayoutOptions importOptions = new Aspose.Cells.Utility.JsonLayoutOptions();
+                importOptions.ConvertNumericOrDate = true;
+                importOptions.ArrayAsTable = true;
+                importOptions.IgnoreArrayTitle = true;
+                importOptions.IgnoreObjectTitle = true; 
+                Aspose.Cells.Utility.JsonUtility.ImportData(rawResponse, cells, 0, 0, importOptions);
+
+                workbook.Save(Path + "\\" + FileName);
+            }
+        }
+
+
+
+
+    }
+}
---
+++ RestAPIProject/RestAPIProject/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Aspose;
+using System.Data;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+
+namespace RestAPIProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var getData = new GetRestData();
+            Console.WriteLine("Enter Path");
+            var dirPath = @"" + Console.ReadLine();
+            Console.WriteLine("Enter the FileName with .csv extension");
+            var FileName = Console.ReadLine();
+
+            string[] tokens = FileName.Split('.');
+            
+            if (tokens[1] == "csv")
+            {
+                if (Directory.Exists(dirPath))
+                {
+                    var path = new DirectoryInfo(dirPath);
+                    getData.getApiData(path.ToString(), FileName);
+                }
+
+                else
+                {
+                    Console.WriteLine("The Path Provided is Incorrect!!!");
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Please Provide the FileName with Correct Input.");
+            }
+        }
+
+    }
+}
